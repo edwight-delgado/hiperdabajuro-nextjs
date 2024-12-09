7f3b4d8349c78611683228f872511534d2de4634
edwight <edwightdelgado@gmail.com>
2024-12-08 22:06:34 -0400

    correjir errores 14

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  missingSuspenseWithCSRBailout: false,
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
 };
 
 export default nextConfig;
